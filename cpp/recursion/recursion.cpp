f7439ffec078c25a77a1df6b1dbf5069f844fc84
Manish Verma <vermam318@gmail.com>
2021-03-13 16:24:47 +0530

    head tail recursion

--- cpp/recursion/recursion.cpp
+++ cpp/recursion/recursion.cpp
@@ -2,18 +2,30 @@
 
 using namespace std;
 
-void fun(int n)
+// tail recursion 
+void fun1(int n)
 {
     if (n > 0)
     {
         printf("%d ",n);
-        fun(n-1);
+        fun1(n-1); // -> ascending output -> print at calling time
+    }
+}
+
+// head recursion  
+void fun2(int n)
+{
+    if (n > 0)
+    {
+        fun2(n-1); // -> descending output -> print at returning time
+        printf("%d ",n);
     }
 }
 
 int main()
 {
     int x = 10;
-    fun(x);
+    fun1(x);
+    fun2(x);
     return 0;
 }
