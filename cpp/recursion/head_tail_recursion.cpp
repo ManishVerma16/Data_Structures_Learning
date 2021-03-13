b0baaeb1dda0c36a48d756514aace4e3541bb22e
Manish Verma <vermam318@gmail.com>
2021-03-13 17:59:47 +0530

    head recursion

--- cpp/recursion/head_tail_recursion.cpp
+++ cpp/recursion/head_tail_recursion.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-// tail recursion 
-void fun1(int n)
-{
-    if (n > 0)
-    {
-        printf("%d ",n);
-        fun1(n-1); // 
-    }
-}
 
-// head recursion  
+/*
+Head recursion -> when there is no operation done before function call or
+function call is the first statement of recursive function block.
+
+All the other operation execute after the function calls.
+
+It means function does not perform anything before calling but it execute everything 
+at returning time.
+
+*/
 void fun2(int n)
 {
     if (n > 0)
     {
-        fun2(n-1); // -> descending output -> print at returning time
+        fun2(n-1); 
         printf("%d ",n);
     }
 }
@@ -25,7 +25,6 @@ void fun2(int n)
 int main()
 {
     int x = 10;
-    fun1(x);
     fun2(x);
     return 0;
 }
