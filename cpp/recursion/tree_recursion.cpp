a77ed6969b8e7ab18438ac51ba3067e8f0702ba2
Manish Verma <vermam318@gmail.com>
2021-03-13 18:32:20 +0530

    tree recursion

---
+++ cpp/recursion/tree_recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Tree recursion -> when there is recursive function calling is happening more than one time.
+
+
+*/
+void fun1(int n)
+{
+    if (n > 0)
+    {
+        printf("%d ",n);
+        fun1(n-1);
+        fun1(n-1); 
+    }
+}
+
+
+int main()
+{
+    int x = 10;
+    fun1(x);
+    return 0;
+}
--- python/recursion/test.py
+++ python/recursion/test.py
@@ -1,7 +1,9 @@
+#  Recursive program to find sum of natural number
+
 def fun(n):
     if n>0:
-        print(n)
-        fun(n-1)
+        return fun(n-1)+n
+    else: return 0
 
 n = int(input())
-fun(n)
+print(fun(n))
