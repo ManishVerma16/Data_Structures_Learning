b9714f7c626bf1b20e1fd19c8f4327d3f7a3e2d3
Manish Verma <vermam318@gmail.com>
2021-03-14 08:40:24 +0530

    factorial of n natural numbers

---
+++ cpp/recursion/factorial_of_n_numbers.cpp
@@ -0,0 +1,20 @@
+// Factorial of n natural numbers
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return fact(n-1)*n;
+    }
+}
+
+int main(){
+    int x;
+    cin >> x;
+    cout << fact(x) << endl;
+    return 0;
+}
